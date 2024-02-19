6fd0930baafdbab5b98e4b038acab4695c478f33
Kimmy-AI <rpmckg@gmail.com>
2024-02-19 15:50:43 +0800

    feat: add demo

---
+++ produce_image.py
@@ -0,0 +1,9 @@
+from diffusers import DiffusionPipeline
+
+pipeline = DiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", use_safetensors=True
+)
+image = pipeline("An image of a squirrel in Picasso style").images[0]
+image
+
+image.save("image_of_squirrel_painting.png")
