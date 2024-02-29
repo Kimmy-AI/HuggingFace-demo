7b092572b3d06cee0667f7e7a1d9a665c396865c
Kimmy-AI <rpmckg@gmail.com>
2024-03-01 00:22:07 +0800

    chore: optimize

---
+++ README.md
@@ -0,0 +1,9 @@
+# HuggingFace-demo
+Generate images from text
+
+## Usage
+Install and run the code:
+```
+pip3 install -r requirements.txt 
+python3 produce_image.py
+```
--- produce_image.py
+++ produce_image.py
@@ -4,6 +4,5 @@ pipeline = DiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5", use_safetensors=True
 )
 image = pipeline("An image of a squirrel in Picasso style").images[0]
-image
 
 image.save("image_of_squirrel_painting.png")
